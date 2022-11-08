61797f73fbcc0a960b49be1b77cdac25f48a20c0
Ray-Xavier-2021 <rayxavier.frontenddev.2021@gmail.com>
2022-11-08 13:36:37 -0500

    completed basic functionality

--- xaviers_list/my_app/views.py
+++ xaviers_list/my_app/views.py
@@ -3,6 +3,16 @@ import requests
 
 from bs4 import BeautifulSoup
 
+# TBD
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from requests.compat import quote_plus
+
+from . import models
+
+BASE_CRAIGSLIST_URL = 'https://newyork.craigslist.org/search/?query={}#search=1~gallery~0~0'
+BASE_IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg'
+
 # Create your views here.
 def home(request):
   template = 'base.html'
@@ -13,13 +23,56 @@ def new_search(request):
 
   # Pull data from search bar
   search = request.POST.get('search')
+  models.Search.objects.create(search=search)
+  final_url = BASE_CRAIGSLIST_URL.format(quote_plus(search))
 
-  # print(search)
+  # configure browser to run JavaScript
+  browser = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+  browser.get(final_url)
+  # print(browser.page_source)
+  response = requests.get(final_url)
+  data = response.text
+  html = browser.page_source
+
+  # Create soup object
+  soup = BeautifulSoup(html, features='html.parser')
+  # browser.close()
+  
+  post_listings = soup.find_all('li', {'class': 'cl-search-result'})
+  # post_title = post_listings[0].find(class_='titlestring').text
+  # post_url = post_listings[0].find('a').get('href')
+  # post_price = post_listings[0].find(class_='priceinfo')
+
+  final_postings = []
+
+  # loop through each post and grab the data
+  for post in post_listings:
+    post_title = post.find(class_='titlestring').text
+    post_url = post.find('a').get('href')
+    
+
+    # Check if there is a price for display if so, display if not N/A
+    if post.find(class_='priceinfo'):
+      post_price = post.find(class_='priceinfo').text
+    else:
+      post_price = 'N/A'
+
+      # print(post_price)
+    # Check if there is an image for display if so, display if not alternate
+    if post.find('img'):
+      post_image_id = post.find('img').get('src').split()
+      post_image_url = BASE_IMAGE_URL.format(post_image_id)
+      print(post_image_url)
+    else:
+      post_image_url = 'https://craigslist.org/images/peace.jpg'
+  
+    # Append tuple of data to list
+    final_postings.append((post_title, post_url, post_price))
 
   # Things passed to frontend
   frontend = {
     'search': search,
+    'final_postings': final_postings,
   }
-
 
   return render(request, search_template, frontend)
