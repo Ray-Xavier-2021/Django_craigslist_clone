86305f4c77a422b1b6f0bedcdc5de8014683f7d8
Ray-Xavier-2021 <rayxavier.frontenddev.2021@gmail.com>
2022-11-07 21:59:22 -0500

    added frontend and modified files

--- xaviers_list/my_app/views.py
+++ xaviers_list/my_app/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
+import requests
+
+from bs4 import BeautifulSoup
 
 # Create your views here.
 def home(request):
   template = 'base.html'
   return render(request, template)
+
+def new_search(request):
+  search_template = 'my_app/new_search.html'
+
+  # Pull data from search bar
+  search = request.POST.get('search')
+
+  # print(search)
+
+  # Things passed to frontend
+  frontend = {
+    'search': search,
+  }
+
+
+  return render(request, search_template, frontend)
